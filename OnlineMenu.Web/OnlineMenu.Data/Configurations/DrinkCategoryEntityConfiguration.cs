4c9005549c968fb51d711ffa67c5ef60ba2fa6dc
nikolaiandrov <nikolaiandrov@abv.bg>
2024-01-04 14:59:57 +0200

    Drink Categories - Seed in config added

--- OnlineMenu.Web/OnlineMenu.Data/Configurations/DrinkCategoryEntityConfiguration.cs
+++ OnlineMenu.Web/OnlineMenu.Data/Configurations/DrinkCategoryEntityConfiguration.cs
@@ -11,6 +11,57 @@
             builder.HasMany(dc => dc.Drinks)
                 .WithOne(d => d.DrinkCategory)
                 .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasData(this.CreateDrinkCategories());
+        }
+
+        private DrinkCategory[] CreateDrinkCategories()
+        {
+            ICollection<DrinkCategory> drinkCategories = new HashSet<DrinkCategory>();
+
+            DrinkCategory category;
+
+            category = new DrinkCategory
+            {
+                Id = 1,
+                Name = "Soft Drinks",
+                IsDeleted = false
+            };
+            drinkCategories.Add(category);
+
+            category = new DrinkCategory
+            {
+                Id = 2,
+                Name = "Hot Drinks",
+                IsDeleted = false
+            };
+            drinkCategories.Add(category);
+
+            category = new DrinkCategory
+            {
+                Id = 3,
+                Name = "Whisky",
+                IsDeleted = false
+            };
+            drinkCategories.Add(category);
+
+            category = new DrinkCategory
+            {
+                Id = 4,
+                Name = "Beer",
+                IsDeleted = false
+            };
+            drinkCategories.Add(category);
+
+            category = new DrinkCategory
+            {
+                Id = 5,
+                Name = "Wine",
+                IsDeleted = false
+            };
+            drinkCategories.Add(category);
+
+            return drinkCategories.ToArray();
         }
     }
 }
