37023d9d077946d65831f6f929f0172faa0e2601
nikolaiandrov <nikolaiandrov@abv.bg>
2024-01-24 20:05:36 +0200

    DrinkServiceTests created and ready to use

---
+++ OnlineMenu.Web/OnlineMenu.Services.Tests/DrinkServiceTests.cs
@@ -0,0 +1,53 @@
+﻿namespace OnlineMenu.Services.Tests
+{
+    using Microsoft.EntityFrameworkCore;
+    using OnlineMenu.Data;
+	using OnlineMenu.Services.Interfaces;
+    using static InMemoryDatabaseSeeder;
+
+	[TestFixture]
+	public class DrinkServiceTests
+	{
+        private DbContextOptions<OnlineMenuDbContext> dbOptions;
+        private OnlineMenuDbContext dbContext;
+        private IDrinkService drinkService;
+
+        public DrinkServiceTests()
+        {
+            
+        }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            this.dbOptions = new DbContextOptionsBuilder<OnlineMenuDbContext>()
+                .UseInMemoryDatabase("OnlineMenuInMemory" + Guid.NewGuid().ToString())
+                .Options;
+
+            this.dbContext = new OnlineMenuDbContext(this.dbOptions);
+            await this.dbContext.Database.EnsureCreatedAsync();
+            Seed(dbContext);
+
+            this.drinkService = new DrinkService(this.dbContext);
+        }
+
+        [Test]
+        public async Task IsDrinkExistingByIdAsync_ShouldReturnFalseWhenInvalidIdPassed()
+        {
+            string drinkId = "someinvalidid123123-abababa";
+            bool isDrinkExisting = await this.drinkService.IsDrinkExistingByIdAsync(drinkId);
+
+            Assert.IsFalse(isDrinkExisting);
+        }
+
+
+
+
+		[TearDown] 
+        public async Task TearDown()
+        {
+            await this.dbContext.Database.EnsureCreatedAsync();
+            await this.dbContext.DisposeAsync();
+        }
+    }
+}
