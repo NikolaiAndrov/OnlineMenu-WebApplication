1e22280bbe7e8ab3cd31efd79eb389024101dc86
nikolaiandrov <nikolaiandrov@abv.bg>
2024-01-25 21:10:08 +0200

    DeleteCategoryAsync tested

--- OnlineMenu.Web/OnlineMenu.Services.Tests/DrinkCategoryServiceTests.cs
+++ OnlineMenu.Web/OnlineMenu.Services.Tests/DrinkCategoryServiceTests.cs
@@ -2,8 +2,10 @@
 {
 	using Microsoft.EntityFrameworkCore;
 	using OnlineMenu.Data;
+	using OnlineMenu.Data.Models;
 	using OnlineMenu.Services.Interfaces;
 	using OnlineMenu.Web.ViewModels.DrinkCategory;
+	using static Common.GeneralApplicationMessages;
 
 	[TestFixture]
 	public class DrinkCategoryServiceTests
@@ -44,6 +46,19 @@
 			Assert.IsTrue(isCategoryExisting);
 		}
 
+		[Test]
+		public async Task DeleteCategoryAsync_ShouldSoftDeleteGivenCategory()
+		{
+			DrinkCategory? drinkCategory = await this.dbContext.DrinksCategories
+				.FirstOrDefaultAsync(dc => dc.IsDeleted == false);
+			Assert.IsNotNull(drinkCategory, ItemNotFoundTestMessage);
+
+			await this.drinkCategoryService.DeleteCategoryAsync(drinkCategory.Id);
+			bool isCategoryExisting = await this.dbContext.DrinksCategories
+				.AnyAsync(dc => dc.IsDeleted == false && dc.Id == drinkCategory.Id);
+
+			Assert.IsFalse(isCategoryExisting);
+		}
 
 		[TearDown]
 		public async Task TearDown()
