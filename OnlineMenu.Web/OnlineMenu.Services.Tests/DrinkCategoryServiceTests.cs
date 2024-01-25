bf2396e41a1da9e3d026c28cb04941d109142889
nikolaiandrov <nikolaiandrov@abv.bg>
2024-01-25 20:55:19 +0200

    DrinkCategoryServiceTests ready to use

---
+++ OnlineMenu.Web/OnlineMenu.Services.Tests/DrinkCategoryServiceTests.cs
@@ -0,0 +1,40 @@
+﻿namespace OnlineMenu.Services.Tests
+{
+	using Microsoft.EntityFrameworkCore;
+	using OnlineMenu.Data;
+	using OnlineMenu.Services.Interfaces;
+
+	[TestFixture]
+	public class DrinkCategoryServiceTests
+	{
+		private DbContextOptions<OnlineMenuDbContext> dbOptions;
+		private OnlineMenuDbContext dbContext;
+		private IDrinkCategoryService drinkCategoryService;
+
+		public DrinkCategoryServiceTests()
+        {
+            
+        }
+
+		public async Task SetUp()
+		{
+			this.dbOptions = new DbContextOptionsBuilder<OnlineMenuDbContext>()
+				.UseInMemoryDatabase("OnlineMenuInMemory" + Guid.NewGuid().ToString())
+				.Options;
+
+			this.dbContext = new OnlineMenuDbContext(this.dbOptions);
+			await this.dbContext.Database.EnsureCreatedAsync();
+			this.drinkCategoryService = new DrinkCategoryService(dbContext);
+		}
+
+
+
+
+		[TearDown]
+		public async Task TearDown()
+		{
+			await this.dbContext.Database.EnsureDeletedAsync();
+			await this.dbContext.DisposeAsync();
+		}
+	}
+}
